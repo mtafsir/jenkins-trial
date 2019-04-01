318fba05be21ab43ed44081306bd26c8fd8c7035
mtafs <mtafs@192.168.0.6>
2019-04-01 00:28:38 -0500

    New build

--- test3/src/main/java/com/Test/demo/user/User.java
+++ test3/src/main/java/com/Test/demo/user/User.java
@@ -1,4 +1,4 @@
-package User;
+package com.Test.demo.user;
 
 import java.util.Date;
 
--- test3/src/main/java/com/Test/demo/user/userDataService.java
+++ test3/src/main/java/com/Test/demo/user/userDataService.java
@@ -1,4 +1,4 @@
-package User;
+package com.Test.demo.user;
 
 import java.util.ArrayList;
 import java.util.Date;
